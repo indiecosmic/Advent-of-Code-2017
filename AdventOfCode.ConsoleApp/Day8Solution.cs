f08ff93786c89af775c92c6c9ce69a3f12a17cc4
Mattias Rudén <mattias@indiesoft.org>
2017-12-10 18:25:01 +0100

    Add solution for day 8

--- AdventOfCode.ConsoleApp/Day8Solution.cs
+++ AdventOfCode.ConsoleApp/Day8Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +12,33 @@ namespace AdventOfCode.ConsoleApp
     {
         public static void Run()
         {
-            var input = @"b inc 5 if a > 1
-                          a inc 1 if b < 5
-                          c dec -10 if a >= 1
-                          c inc -20 if c == 10";
+            //var input = @"b inc 5 if a > 1
+            //              a inc 1 if b < 5
+            //              c dec -10 if a >= 1
+            //              c inc -20 if c == 10";
+            string input;
+            using (var reader = new StreamReader("day8input.txt"))
+            {
+                input = reader.ReadToEnd();
+            }
+
             var instructions = ParseInstructions(input);
             var register = InitializeRegisters(instructions);
+            int max = 0;
             foreach (var instruction in instructions)
             {
-                register[instruction.TargetRegister] = instruction.ApplyTo(register[instruction.TargetRegister], register[instruction.ConditionRegister]);
+                var value = instruction.ApplyTo(register[instruction.TargetRegister], register[instruction.ConditionRegister]);
+                if (value > max)
+                    max = value;
+                register[instruction.TargetRegister] = value;
             }
+            var maxValue = register.Max(pair => pair.Value);
+            Console.WriteLine($"Max value: {maxValue} ({max})");
         }
 
         public static IList<JumpInstruction> ParseInstructions(string input)
         {
-            var instructions = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var instructions = input.Split(new[] { "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
             instructions = instructions.Select(i => i.Trim()).ToArray();
             return instructions.Select(JumpInstruction.Parse).ToList();
         }
