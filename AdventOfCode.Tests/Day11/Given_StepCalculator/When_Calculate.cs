e12503b92e37c1d2ac46434786bc3ab3e048b00a
Mattias Rudén <mattias@indiesoft.org>
2017-12-11 16:27:31 +0100

    Draft for day 11

---
+++ AdventOfCode.Tests/Day11/Given_StepCalculator/When_Calculate.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode.Day11;
+using AdventOfCodeTests;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Day11.Given_StepCalculator
+{
+    [TestFixture]
+    public class When_Calculate : TestBase<StepCalculator>
+    {
+        [TestCase("ne, ne, ne", 3)]
+        [TestCase("ne, ne, sw, sw", 0)]
+        [TestCase("ne, ne, s, s", 2)]
+        [TestCase("se, sw, se, sw, sw", 3)]
+        public void Result_should_be_least_steps_needed(string input, int expectedResult)
+        {
+            var result = Subject.CalculateFewestNumberOfSteps(input);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
