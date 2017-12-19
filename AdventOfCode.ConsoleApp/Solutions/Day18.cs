15bd5047d16ae51baf0c2c5704940694cacf31a5
Mattias Rudén <mattias@indiesoft.org>
2017-12-19 21:46:54 +0100

    Day 18 part 2

--- AdventOfCode.ConsoleApp/Solutions/Day18.cs
+++ AdventOfCode.ConsoleApp/Solutions/Day18.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using AdventOfCode.Day18;
 
 namespace AdventOfCode.ConsoleApp.Solutions
@@ -10,34 +11,69 @@ namespace AdventOfCode.ConsoleApp.Solutions
         {
             Console.WriteLine("Day 18");
             var input = GetInput();
-            //var input = "set a 1\nadd a 2\nmul a a\nmod a 5\nsnd a\nset a 0\nrcv a\njgz a -1\nset a 1\njgz a -2";
-
 
             var rows = input.Split(new[] { ',', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
             var instructions = rows.Select(Instruction.Parse).ToArray();
-            var state = new Duet();
+            Part1(instructions);
+            Part2(instructions);
+        }
+
+        private void Part1(Instruction[] instructions)
+        {
+            var state = new Duet(0);
             foreach (var instruction in instructions)
             {
-                if (!state.Registers.ContainsKey(instruction.RegisterName))
+                if (!state.Registers.ContainsKey(instruction.RegisterName) && Regex.IsMatch(instruction.RegisterName, "^[a-z]*$"))
                     state.Registers[instruction.RegisterName] = 0;
             }
 
+            Console.WriteLine("Part 1");
             var index = 0;
             while (index >= 0 && index < instructions.Length)
             {
                 var instruction = instructions[index];
+                index += instruction.Execute(state);
 
                 if (instruction is Rcv)
                 {
-                    
+                    break;
                 }
-
-                index += instruction.Execute(state);
             }
 
+            Console.WriteLine($"Result {state.Messages.Last()}");
+        }
 
+        private void Part2(Instruction[] instructions)
+        {
+            Console.WriteLine("Part 2");
+            var state1 = new Duet(0);
+            var state2 = new Duet(1);
+            state1.Connect(state2);
+            foreach (var instruction in instructions)
+            {
+                if (!state1.Registers.ContainsKey(instruction.RegisterName) && Regex.IsMatch(instruction.RegisterName, "^[a-z]*$"))
+                    state1.Registers[instruction.RegisterName] = 0;
+                if (!state2.Registers.ContainsKey(instruction.RegisterName) && Regex.IsMatch(instruction.RegisterName, "^[a-z]*$"))
+                    state2.Registers[instruction.RegisterName] = 0;
+            }
 
-            Console.WriteLine("Part 1");
+            var index1 = 0;
+            var index2 = 0;
+            while (index1 >= 0 && index1 < instructions.Length && index2 >= 0 && index2 < instructions.Length)
+            {
+                var instruction1 = instructions[index1];
+                index1 += instruction1.Execute(state1);
+
+                var instruction2 = instructions[index2];
+                index2 += instruction2.Execute(state2);
+                if (state1.Waiting && instruction1 is Rcv && state2.Waiting && instruction2 is Rcv)
+                {
+                    Console.WriteLine("Deadlock");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Messages sent: {state1.MessagesSent}");
         }
     }
 }
