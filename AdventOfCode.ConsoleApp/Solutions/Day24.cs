e938d56dbe6061619fbbfaf937e2846c3ac5faa8
Mattias Rudén <mattias@indiesoft.org>
2017-12-27 21:15:23 +0100

    Day 24 part 2

--- AdventOfCode.ConsoleApp/Solutions/Day24.cs
+++ AdventOfCode.ConsoleApp/Solutions/Day24.cs
@@ -29,6 +29,17 @@ namespace AdventOfCode.ConsoleApp.Solutions
             }
 
             Console.WriteLine($"Max strength: {max}");
+
+            Console.WriteLine("Part 2");
+            var maxLength = (0,0);
+            foreach (var start in startComponents)
+            {
+                var pinIndex = Array.IndexOf(start.Ports, 0) == 0 ? 1 : 0;
+                var nextPin = start.Ports[pinIndex];
+                var strength = BuildLongBridge(nextPin, start, components.Except(new[] { start }).ToList());
+                if (strength.Item1 > maxLength.Item1 || (strength.Item1 == maxLength.Item1 && strength.Item2 > maxLength.Item2)) maxLength = strength;
+            }
+            Console.WriteLine($"Longest: {maxLength.Item1}, strength: {maxLength.Item2}");
         }
 
         public int BuildBridge(int pin, Component current, List<Component> components)
@@ -43,6 +54,23 @@ namespace AdventOfCode.ConsoleApp.Solutions
                 var nextPin = component.Ports[pinIndex];
                 var strength = current.Strength + BuildBridge(nextPin, component, components.Except(new []{ component}).ToList());
                 if (strength > max) max = strength;
+            }
+            return max;
+        }
+
+        public (int,int) BuildLongBridge(int pin, Component current, List<Component> components)
+        {
+            var compatible = components.Where(c => c.Ports.Any(p => p == pin)).ToList();
+            if (compatible.Count == 0) return (1,current.Strength);
+
+            var max = (0,0);
+            foreach (var component in compatible)
+            {
+                var pinIndex = Array.IndexOf(component.Ports, pin) == 0 ? 1 : 0;
+                var nextPin = component.Ports[pinIndex];
+                var res = BuildLongBridge(nextPin, component, components.Except(new[] { component }).ToList());
+                var strength = (1 + res.Item1, current.Strength + res.Item2);
+                if (strength.Item1 > max.Item1 || (strength.Item1 == max.Item1 && strength.Item2 > max.Item2)) max = strength;
             }
             return max;
         }
